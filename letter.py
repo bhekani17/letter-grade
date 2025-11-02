5e991acccbe2e7a1d8f8f5e94592129915d70d23
Bhekani17 <ndlovubhekani17@gmail.com>
2025-11-02 07:50:17 +0200

    updated

--- letter.py
+++ letter.py
@@ -1,4 +1,17 @@
 def letter_grade(mark: int) -> str:
+
+# Write a function called letter_grade(mark:int) -> str that returns a 
+# letter a student got: 
+# ○ A = 80–100 
+# ○ B = 70–79 
+# ○ C = 60–69 
+# ○ D = 50–59 
+# ○ F = below 50 
+# Write unit tests to test if the function returns the expected. 
+# the file structure of the program must be 
+# letter.py 
+# test_letter.py
+
     if mark < 0 or mark > 100:
         raise ValueError("Mark must be between 0 and 100")
     elif mark >= 80:
